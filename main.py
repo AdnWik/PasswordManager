d724cd0ca5f286e3f7bfeba57f6725d5e27277da
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-12-16 21:27:42 +0100

    Initial setup for sqlalchemy and tkinter

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print('Hello World !!!')
+import yaml
+from sqlalchemy import (
+    create_engine,
+    Table,
+    Column,
+    Integer,
+    String,
+    MetaData
+    )
+
+
+def load_config(filename) -> dict:
+    """Load config from YAML file
+
+    Args:
+        filename (str): _filename_.yaml
+
+    Returns:
+        dict: dictionary with configuration
+    """
+    with open(filename, encoding='utf-8') as file:
+        data = yaml.safe_load(file)
+    return data
+
+
+config = load_config('config.yaml')
+engine = create_engine('sqlite:///' + config['sqlalchemy']['database_name'],
+                       echo=config['sqlalchemy']['echo'],
+                       future=config['sqlalchemy']['future']
+                       )
+meta = MetaData()
+
+services = Table(
+    'services', meta,
+    Column('id', Integer, primary_key=True),
+    Column('service_name', String)
+)
+
+credentials = Table(
+    'credentials', meta,
+    Column('id', Integer, primary_key=True),
+    Column('service_id', Integer),
+    Column('login', String),
+    Column('password', String)
+)
+
+meta.create_all(engine)
