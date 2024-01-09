1e6a9d22d4c2156c284f75c86080844ded3de424
Adrian Wójcik <wojcik.adrian@protonmail.com>
2024-01-09 19:47:49 +0100

    Add test table

--- gui.py
+++ gui.py
@@ -28,23 +28,31 @@ tabsystem.grid(column=0, row=0,)
 
 #frm = ttk.Frame(root, padding=10)
 #frm.grid()
-login_label = ttk.Label(tab1, text="Login")
+
+tree = ttk.Treeview(tab1, columns=('id', 'service', 'login', 'password'), show='headings', height=8)
+tree.heading('id', text='id')
+tree.heading('service', text='service')
+tree.heading('login', text='login')
+tree.heading('password', text='password')
+tree.grid(column=0, row=0)
+
+login_label = ttk.Label(tab2, text="Login")
 login_label.grid(column=1, row=1, pady=5, padx=5)
 
-login_entry = ttk.Entry(tab1)
+login_entry = ttk.Entry(tab2)
 login_entry.grid(column=2, row=1)
 
-password_label = ttk.Label(tab1, text="Password")
+password_label = ttk.Label(tab2, text="Password")
 password_label.grid(column=1, row=2, pady=5, padx=5)
 
-password_entry = ttk.Entry(tab1)
+password_entry = ttk.Entry(tab2)
 password_entry.grid(column=2, row=2)
 
-button_sing_in = ttk.Button(tab1, text="Sing In")
+button_sing_in = ttk.Button(tab2, text="Sing In")
 button_sing_in.grid(column=0, row=3)
 button_sing_in.bind('<Button-1>', on_click_button_sing_in)
 
-button_quit = ttk.Button(tab1, text="Quit")
+button_quit = ttk.Button(tab2, text="Quit")
 button_quit.grid(column=1, row=3)
 button_quit.bind('<Button-1>', on_click_button_quit)
 
